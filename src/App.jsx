2229a56cf4734b3701cc44c436044cecbc8011fd
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-01-22 12:21:59 +0800

    basic routes/route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Invoices from './components/Invoices';
+import Expenses from './components/Expenses';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/invoices' element={<Invoices />} />
+        <Route path='/expenses' element={<Expenses />} />
+      </Routes>
     </>
   );
 }
