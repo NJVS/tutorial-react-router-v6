269f841ebf9d5f5242082a31d1e251cc0915d38f
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-01-25 00:15:38 +0800

    display invoice details

--- src/components/Bills.jsx
+++ src/components/Bills.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { getInvoice } from '../data';
 
 const Bills = () => {
   const { invoiceID } = useParams();
+  const invoice = getInvoice(+invoiceID);
+
+  const toCurrency = new Intl.NumberFormat('en-us', {
+    currency: 'USD',
+    style: 'currency'
+  })
 
   return (
-    <div className='invoice-item'>Invoice No.{invoiceID}</div>
+    <div className='invoice-item'>
+      <h3>{invoice.number}</h3>
+      <h2>{invoice.name}</h2>
+      <p>Total due: <span>{toCurrency.format(invoice.amount)}</span></p>
+      <p>Due date: <span>{invoice.due}</span></p>
+    </div>
   )
 }
 
