9d6c95229ffad049c5e045273934b25f89e0bc2d
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-01-24 21:05:56 +0800

    invoice data

--- src/components/Invoices.jsx
+++ src/components/Invoices.jsx
@@ -1,40 +1,11 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-
-const invoices = [
-  {
-    name: 'Santa Monica',
-    number: 1995,
-    amount: 10800,
-    due: '12/05/1995'
-  },
-  {
-    name: 'Stankonia',
-    number: 2000,
-    amount: 8000,
-    due: '10/31/2000'
-  },
-  {
-    name: 'Ocean Avenue',
-    number: 2003,
-    amount: 9500,
-    due: '07/22/2003'
-  },
-  {
-    name: 'Tubthumper',
-    number: 1997,
-    amount: 14000,
-    due: '09/01/1997'
-  },
-  {
-    name: 'Wide Open Spaces',
-    number: 1998,
-    amount: 4600,
-    due: '01/27/1998'
-  }
-]
+import { getInvoices } from '../data';
 
 const Invoices = () => {
+
+  const invoices = getInvoices();
+
   return (
     <main className='invoices container'>
       <aside>
